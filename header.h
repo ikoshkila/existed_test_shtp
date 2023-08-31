079c3d6034a5a6f998a75538fe0fd3b4ef9786c0
ikoshkila <114185021+ikoshkila@users.noreply.github.com>
2023-08-31 11:44:24 -1000

    Delete header.h

--- header.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-
-void odd_even(const std::vector<int> &lst, std::vector<int> &lst1, std::vector<int> &lst2);
-void pos_neg(const std::vector<int> &lst, std::vector<int> &lst1, std::vector<int> &lst2, std::vector<int> &lst3);
